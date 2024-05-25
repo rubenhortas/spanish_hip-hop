fedbdc4bd5f6a4b4203612d0be746551d2ed74b6
rubenhortas <rubenhortas@gmail.com>
2024-05-25 13:47:57 +0200

    Refactoring

--- tools/standarize/standarize.py
+++ tools/standarize/standarize.py
@@ -4,9 +4,10 @@ import os
 import signal
 from types import FrameType
 
-from tools.capitalize.Album import Album
+from Album import Album
 
 CSV_FILE = 'lista trabajos hip-hop español.csv'
+CSV_OUTPUT_FILE = f"{CSV_FILE[:-4]} - formateado.csv"
 CSV_SEPARATOR = ','
 
 
@@ -28,11 +29,12 @@ def _format_entries() -> list:
 
     for entry in original_entries:
         entry_ = entry.split(CSV_SEPARATOR)
-        album = Album(entry_[0], entry_[1], entry_[2], entry_[3], CSV_SEPARATOR)
-        formatted_entries.append(f"{str(album)}\n")
+        album = Album(entry_[0], entry_[1], entry_[2], entry_[3], CSV_SEPARATOR)  # artist, title, date, format
+        formatted_entries.append(album.list())
 
-    # TODO: Sort list by artist/year/title before return
-    return formatted_entries
+    sorted(formatted_entries, key=lambda album: (album[0], album[2], album[1]))
+
+    return [f"{'CSV_SEPARATOR'.join(e)}\n" for e in formatted_entries]
 
 
 def _read_file() -> list:
@@ -50,18 +52,18 @@ def _read_file() -> list:
         exit(-1)
 
 
-def _write_output_file(file: str, lines: list) -> None:
+def _write_output_file(lines: list) -> None:
     try:
-        with open(file, 'w') as f:
+        with open(CSV_OUTPUT_FILE, 'w') as f:
             f.writelines(lines)
     except FileNotFoundError as file_not_found_error:
         print(f"'{file_not_found_error.filename}' no such file or directory")
         exit(-1)
     except PermissionError:
-        print(f"Permission denied: '{file}'")
+        print(f"Permission denied: '{CSV_OUTPUT_FILE}'")
         exit(-1)
     except OSError as os_error:
-        print(f"'{file}' OSError: {os_error}")
+        print(f"'{CSV_OUTPUT_FILE}' OSError: {os_error}")
         exit(-1)
 
 
@@ -69,4 +71,4 @@ if __name__ == '__main__':
     signal.signal(signal.SIGINT, _handle_sigint)
     _clear_screen()
     formatted_entries = _format_entries()
-    _write_output_file('new.csv', formatted_entries)
+    _write_output_file(formatted_entries)
