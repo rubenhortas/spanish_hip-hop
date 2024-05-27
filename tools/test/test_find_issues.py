27644e3360c5b3aab4637a31ce09e5c569447c9f
rubenhortas <rubenhortas@gmail.com>
2024-05-27 21:29:34 +0200

    Refactoring

--- tools/test/test_find_issues.py
+++ tools/test/test_find_issues.py
@@ -6,20 +6,20 @@ from tools.find_issues import _get_issues
 class TestFindIssues(unittest.TestCase):
     def setUp(self):
         self.lines = [
-            'Artista,Trabajo,Fecha Publicación, Ok\n',
-            'Artista,Trabajo,Fecha Publicación,Extra separator,\n',
-            'Artista,Trabajo,Fecha (Publicación,Parentheses issue\n',
-            'Artista,Trabajo,Fecha (Publicación,Extra separator + Parentheses issue,\n'
+            'Bob,Album1,2024, Ok\n',
+            'Bob,Album1,2024,Extra separator,\n',
+            'Bob,(Album1,2024,Parentheses issue\n',
+            'Bob,(Album1,2024,Extra separator + Parentheses issue,\n'
         ]
 
         self.extra_separators_expected = [
-            'Artista,Trabajo,Fecha Publicación,Extra separator,\n',
-            'Artista,Trabajo,Fecha (Publicación,Extra separator + Parentheses issue,\n'
+            'Bob,Album1,2024,Extra separator,\n',
+            'Bob,(Album1,2024,Extra separator + Parentheses issue,\n'
         ]
 
         self.parentheses_issues_expected = [
-            'Artista,Trabajo,Fecha (Publicación,Parentheses issue\n',
-            'Artista,Trabajo,Fecha (Publicación,Extra separator + Parentheses issue,\n'
+            'Bob,(Album1,2024,Parentheses issue\n',
+            'Bob,(Album1,2024,Extra separator + Parentheses issue,\n'
         ]
 
     def test_get_issues(self):
