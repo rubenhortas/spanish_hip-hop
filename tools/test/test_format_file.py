0aaa5531265ee2d6a66d3c464d603a0d0587ccfd
rubenhortas <rubenhortas@gmail.com>
2024-05-28 22:37:13 +0200

    Refactoring

---
+++ tools/test/test_format_file.py
@@ -0,0 +1,28 @@
+import unittest
+
+from tools.format_file import Album
+
+
+class TestFormatFile(unittest.TestCase):
+    def setUp(self):
+        self.csv_separator = ','
+        self.albums = [
+            'bob mc & alice dj,-,-,-',
+            'cpv,-,-,-',
+            'bob mc,the album vol. ii,-,-'
+        ]
+        self.expected_result = [
+            'Bob MC & Alice DJ,-,-,-',
+            'CPV,-,-,-',
+            'Bob MC,The album Vol. II,-,-'
+        ]
+
+    def test_format_file(self):
+        result = []
+
+        for entry in self.albums:
+            entry_ = entry.split(self.csv_separator)
+            album = Album(entry_[0], entry_[1], entry_[2], entry_[3], self.csv_separator)  # artist, title, date, format
+            result.append(str(album))
+
+        self.assertEqual(self.expected_result, result)
