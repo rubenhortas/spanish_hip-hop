8346e91b8396011d1f0fa194077b8a3d4af839da
rubenhortas <rubenhortas@gmail.com>
2024-05-30 21:07:49 +0200

    Refactoring

--- tools/format_file.py
+++ tools/format_file.py
@@ -10,41 +10,28 @@ from tools.libraries.album import Album
 CSV_OUTPUT_FILE = f"{CSV_FILE[:-4]} - formateado.csv"
 
 
-def _format_entries() -> list:
-    original_entries = [line.strip() for line in read_file(CSV_FILE)][1:]
-    formatted_entries = []
-    entry_num = 1
-    entries_num = len(original_entries)
+def _get_albums(line: list) -> list:
+    lines_ = [line.strip() for line in line]
+    albums = []
 
-    for entry in original_entries:
-        print(f"\r{entry_num}/{entries_num}", end='')
-
+    for line in lines_:
         try:
-            entry_ = entry.split(CSV_SEPARATOR)
-            album = Album(entry_[0], entry_[1], entry_[2], entry_[3])  # artist, title, date, format
-            formatted_entries.append(album.list())
-
-            # Debug
-            # if entry_[0] != album.artist or entry_[1] != album.title:
-            #     print(f": '{entry_[0]},{entry_[1]}' -> '{album.artist},{album.title}'")
+            line_ = line.split(CSV_SEPARATOR)
+            album = Album(line_[0], line_[1], line_[2], line_[3])  # artist, title, date, format
+            albums.append(album)
         except IndexError:
-            print(f"'{entry}: bad format")
+            print(f"'{line}: bad format")
 
-        entry_num += 1
-
-    print()
-
-    sorted_formatted_entries = sorted(formatted_entries, key=lambda entry: (entry[0], entry[2], entry[1]))
-    result = [f"{CSV_HEADER}\n"]
-    result.extend([f"{CSV_SEPARATOR.join(e)}\n" for e in sorted_formatted_entries])
-
-    return result
+    return sorted(albums)
 
 
 if __name__ == '__main__':
     signal.signal(signal.SIGINT, handle_sigint)
     clear_screen()
     print(f"Generating {CSV_OUTPUT_FILE}")
-    formatted_entries = _format_entries()
-    write_file(CSV_OUTPUT_FILE, formatted_entries)
+    lines = read_file(CSV_FILE)[1:]
+    albums = _get_albums(lines)
+    result = [f"{CSV_HEADER}\n"]
+    result.extend([f"{str(album)}\n" for album in albums])
+    write_file(CSV_OUTPUT_FILE, result)
     print('Done')
--- tools/libraries/album.py
+++ tools/libraries/album.py
@@ -12,8 +12,14 @@ class Album:
     def __str__(self):
         return f"{self.artist}{CSV_SEPARATOR}{self.title}{CSV_SEPARATOR}{self.publication_date}{CSV_SEPARATOR}{self.format}"
 
-    def list(self):
-        return [self.artist, self.title, self.publication_date, self.format]
+    def __eq__(self, other):
+        return self.artist == other.artist and self.publication_date == other.publication_date and self.title == other.title and self.format == other.format
+
+    def __lt__(self, other):
+        return self.artist < other.artist and self.publication_date < other.publication_date and self.title < other.title and self.format < other.format
+
+    def __gt__(self, other):
+        return self.artist > other.artist and self.publication_date > other.publication_date and self.title > other.title and self.format > other.format
 
     def _format_artist(self, artist: str) -> None:
         self.artist = artist.title().strip()
