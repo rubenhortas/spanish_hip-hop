5a06141c8a7ce5eb007b0e8e3a9feb3e56453e49
rubenhortas <rubenhortas@gmail.com>
2024-05-30 22:15:16 +0200

    Refactoring

--- tools/check_exceptions.py
+++ tools/check_exceptions.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+
 from tools.exceptions import EXCEPTIONS
 from tools.libraries.config import CSV_FILE, CSV_SEPARATOR
 from tools.libraries.file_helpers import read_file, write_file
