c5ecd41aa129ae067e65326b39be4bec89b0743b
rubenhortas <rubenhortas@gmail.com>
2024-05-31 14:49:39 +0200

    Refactoring

--- tools/generate_exceptions_dictionary.py
+++ tools/generate_exceptions_dictionary.py
@@ -8,9 +8,9 @@ from tools.libraries.os_helpers import handle_sigint
 OUTPUT_FILE = 'exceptions.py'
 
 
-def _get_exceptions(dictionary: dict):
+def _get_exceptions(dictionary: dict) -> list:
     keys = set()
-    entries = []
+    new_exceptions = []
 
     for key in dictionary:
         keys.add(key)
@@ -18,9 +18,16 @@ def _get_exceptions(dictionary: dict):
     for key in sorted(list(keys), key=str.lower):
         key_ = key.replace("'", "\\'").lower()
         value = dictionary[key].replace("'", "\\'")
-        entries.append(f"\t'{key_}': '{value}',\n")
+        new_exceptions.append(f"\t'{key_}': '{value}',\n")
 
-    return entries
+    return new_exceptions
+
+
+def _write_output_file(keys):
+    exceptions = ['# Words that will be *not* capitalized\n', 'EXCEPTIONS = {\n']
+    exceptions.extend(keys)
+    exceptions.append('}\n')
+    write_file(OUTPUT_FILE, exceptions)
 
 
 if __name__ == '__main__':
@@ -28,8 +35,5 @@ if __name__ == '__main__':
     Generates the file exceptions.py with the exceptions dictionary alphabetically ordered without duplicates.
     """
     signal.signal(signal.SIGINT, handle_sigint)
-    exceptions = ['# Words that will be *not* capitalized\n', 'EXCEPTIONS = {\n']
-    keys = _get_exceptions(EXCEPTIONS)
-    exceptions.extend(keys)
-    exceptions.append('}\n')
-    write_file(OUTPUT_FILE, exceptions)
+    new_exceptions = _get_exceptions(EXCEPTIONS)
+    _write_output_file(new_exceptions)
