0d303217b26bb46a191fb0d942012842d9b40ea4
rubenhortas <rubenhortas@gmail.com>
2024-06-01 20:23:58 +0200

    Refactored

--- tools/libraries/album.py
+++ tools/libraries/album.py
@@ -3,8 +3,8 @@ from tools.libraries.config import CSV_SEPARATOR
 
 class Album:
     def __init__(self, artist: str, title: str, publication_date: str, album_format: str):
-        self._format_artist(artist)
-        self._format_title(title)
+        self.artist = artist.strip()
+        self.title = title.strip()
         self.publication_date = publication_date.strip()
         self.format = album_format.strip().upper()
 
@@ -19,9 +19,3 @@ class Album:
 
     def __gt__(self, other):
         return self.artist > other.artist and self.publication_date > other.publication_date and self.title > other.title and self.format > other.format
-
-    def _format_artist(self, artist: str) -> None:
-        self.artist = artist.strip()
-
-    def _format_title(self, title: str) -> None:
-        self.title = title.strip()
