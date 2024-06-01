86da14ac17482e799d543548e13b156175c1bef6
rubenhortas <rubenhortas@gmail.com>
2024-06-02 00:04:49 +0200

    Refactored

--- tools/get_artists.py
+++ tools/get_artists.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
+
 from tools.libraries.config import CSV_FILE, CSV_SEPARATOR
 from tools.libraries.file_helpers import read_file, write_file
-from tools.libraries.string_utils import replace_exceptions
 
 _OUTPUT_FILE = 'artists.txt'
 
@@ -11,8 +11,11 @@ if __name__ == '__main__':
 
     for line in lines:
         line_ = line.split(CSV_SEPARATOR)
-        if ' & ' not in line_[0] and ' y ' not in line_[0] and '-N-' not in line_[0] and ' vs ' not in line_[0].lower():
+
+        if ' & ' not in line_[0] and ' y ' not in line_[0] and '-N-' not in line_[0] and ' – ' not in line_[0] and ' vs ' not in line_[0].lower():
             artists.add(line_[0].strip().lower())
 
-    artists_ = [f"\t'{replace_exceptions(artist.title())}': '{artist.upper()}',\n" for artist in artists]
+    # artists_ = [f"\t'{replace_exceptions(artist.title())}': '{artist.upper()}',\n" for artist in artists]  # Dictionary format
+    artists_ = [f"\t{artist.title()}\n" for artist in artists]
+
     write_file(_OUTPUT_FILE, sorted(artists_))
