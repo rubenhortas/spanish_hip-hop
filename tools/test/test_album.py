4b156f1f2b2c0fcfa47db0fca1655f533d3958df
rubenhortas <rubenhortas@gmail.com>
2024-06-02 12:42:11 +0200

    Refactored

--- tools/test/test_album.py
+++ tools/test/test_album.py
@@ -4,7 +4,7 @@ from tools.format_file import Album
 from tools.libraries.config import CSV_SEPARATOR
 
 
-class TestFormatFile(unittest.TestCase):
+class TestAlbum(unittest.TestCase):
     def setUp(self):
         self.albums = [
             ('bob mc & alice dj,-,-,-', 'Bob MC & Alice DJ,-,-,-'),
@@ -12,7 +12,7 @@ class TestFormatFile(unittest.TestCase):
             ('bob mc,the album vol. ii,-,-', 'Bob MC,The album Vol. II,-,-')
         ]
 
-    def test_format_file(self):
+    def test_format_album(self):
         for album, expected_result in self.albums:
             album_ = album.split(CSV_SEPARATOR)
             formatted_album = Album(album_[0], album_[1], album_[2], album_[3])  # artist, title, date, format
