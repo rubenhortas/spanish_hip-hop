11e448988d7d2cad9ac55867557ecbea269adbab
rubenhortas <rubenhortas@gmail.com>
2024-06-10 20:59:00 +0200

    Refctored

--- tools/dev/fix_file.py
+++ tools/dev/fix_file.py
@@ -2,82 +2,70 @@
 
 import os
 import signal
+from collections import Counter
 
-from tools.libraries.config import CSV_FILE, CSV_SEPARATOR
+from tools.libraries.config import CSV_FILE, CSV_SEPARATOR, SEPARATOR_NUMBER
 from tools.libraries.file_helpers import read_file, write_file
 from tools.libraries.os_helpers import handle_sigint, clear_screen
 
-_OUTPUT_FILE = f"{CSV_FILE[:-4]} - fixed.csv"
-_FOREIGN_ARTISTS = ['Ace Hood', 'Aqeel', 'Aqueel', 'Asap Mob', 'G Jazz', 'Gavlyn', 'Gee Falcone', 'Jim Jones', 'Kafu Banton', 'Kev Brown',
-                    'Kidz In The Hall', 'Random Axe', 'Red Pill', 'Rick Ross', 'Schoolboy Q', 'Sean Combs', 'Snak The Ripper', 'Stan Forebee',
-                    'Stat Quo', 'Statik Selektah', 'Step Brothers', 'Strange Fruit Project', 'Street Bucks', 'String Theory',
-                    'Strong Arm Steady', 'TNGHT', 'Tenacity', 'Terrace Martin', 'Tragedy Khadafi', 'Vinnie Paz', 'Wadada Sound System', 'Wale',
+_OUTPUT_FILE = f"{CSV_FILE[:-4]} - arreglado.csv"
+_ERROR_FILE = f"{CSV_FILE[:-4]} - errores.csv"
+_FOREIGN_ARTISTS = ['Ace Hood', 'Aqeel', 'Aqueel', 'Asap Mob', 'G Jazz', 'Gavlyn', 'Gee Falcone', 'Jim Jones',
+                    'Kafu Banton', 'Kev Brown',
+                    'Kidz In The Hall', 'Random Axe', 'Red Pill', 'Rick Ross', 'Schoolboy Q', 'Sean Combs',
+                    'Snak The Ripper', 'Stan Forebee',
+                    'Stat Quo', 'Statik Selektah', 'Step Brothers', 'Strange Fruit Project', 'Street Bucks',
+                    'String Theory',
+                    'Strong Arm Steady', 'TNGHT', 'Tenacity', 'Terrace Martin', 'Tragedy Khadafi', 'Vinnie Paz',
+                    'Wadada Sound System', 'Wale',
                     'Wiz Khalifa', 'Young Jeezy']
 _DESCONOCIDOS = ['desconocido', '[desconocido]', 'intérprete desconocido']
 
 
-def _remove_foreign_artists(lines: list) -> list:
-    print('Removing foreign artists...')
-    lines_ = []
-
-    for line in lines:
-        line_ = line.split(CSV_SEPARATOR)
-
-        if line_[1] not in _FOREIGN_ARTISTS:
-            lines_.append(line)
-
-    return lines
-
-
-def _remove_quotation_marks(lines: list) -> list:
-    print('Removing quotation marks...')
+def _fix(lines: list) -> (list, list):
     fixed_lines = []
+    errors = []
 
     for line in lines:
-        fixed_line = line.replace('"', '')
-        fixed_lines.append(fixed_line)
+        if Counter(line)[CSV_SEPARATOR] == SEPARATOR_NUMBER:
+            fields = line.split(CSV_SEPARATOR)
 
-    return fixed_lines
+            if fields[1] not in _FOREIGN_ARTISTS:
+                if fields[9] == '-' or fields[9] == '':  # Is not preserved
+                    fixed_fields = []
 
+                    for field in fields:
+                        field_ = field
 
-def _add_dashes(lines: list) -> list:
-    print('Adding dashes...')
-    fixed_lines = []
+                        if field_:
+                            if field_[0] == '"' and field_[-1] == '"':
+                                field_ = field_[1:-1]
 
-    for line in lines:
-        line_ = line.split(CSV_SEPARATOR)
-        new_line = ','.join([field if field else '-' for field in line_])
-        fixed_lines.append(new_line)
+                            if field_.lower() in _DESCONOCIDOS:
+                                field_ = ''
 
-    return fixed_lines
+                        fixed_fields.append(field_)
 
+                    fixed_lines.append(CSV_SEPARATOR.join(fixed_fields))
+        else:
+            errors.append(line)
 
-def _delete_desconocidos(lines: list) -> list:
-    print('Deleting "desconocido[s]"')
-    fixed_lines = []
-
-    for line in lines:
-        line_ = line.split(CSV_SEPARATOR)
-        new_line = ','.join([field if field.lower() not in _DESCONOCIDOS else '-' for field in line_])
-        fixed_lines.append(new_line)
-
-    return fixed_lines
-
-
-def _fix(lines: list) -> list:
-    fixed_lines = _remove_quotation_marks(lines)
-    fixed_lines = _remove_foreign_artists(fixed_lines)
-    fixed_lines = _add_dashes(fixed_lines)
-    fixed_lines = _delete_desconocidos(fixed_lines)
-
-    return fixed_lines
+    return fixed_lines, errors
 
 
 if __name__ == '__main__':
     signal.signal(signal.SIGINT, handle_sigint)
     clear_screen()
     print(f"Fixing {CSV_FILE}...")
-    lines = read_file(os.path.join(os.path.abspath(''), CSV_FILE))
-    fixed_lines = _fix(lines)
+
+    lines = read_file(os.path.join(os.path.abspath('..'), CSV_FILE))
+    header = lines[0].replace('"', '')
+    fixed_lines, errors = _fix(lines[1:])
+
+    fixed_lines.insert(0, header)
     write_file(os.path.join(os.path.abspath(''), _OUTPUT_FILE), fixed_lines)
+
+    if errors:
+        write_file(os.path.join(os.path.abspath(''), _ERROR_FILE), errors)
+
     print('Done')
