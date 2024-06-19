e761cb7a659b8ead62f5ee35481aed1d7aae5c61
rubenhortas <rubenhortas@gmail.com>
2024-06-19 20:30:18 +0200

    Improved tests

--- tools/tests/test_generate_exceptions_dictionary.py
+++ tools/tests/test_generate_exceptions_dictionary.py
@@ -6,12 +6,13 @@ from tools.regenerate_exceptions_dictionary import _get_exceptions
 class TestGenerateExceptionsDictionary(unittest.TestCase):
     def setUp(self):
         self.dictionary = {
-            'key1': 'KEY1',
-            'key1': 'KEY1',  # duplicated key
-            'key2': 'KEY2'
+            'key1': 'KEY11',
+            'key1': 'KEY1',  # Duplicated key
+            'key2': 'KEY2',
+            'foo\'s bar': 'FOO\'S BAR'  # Escaped chars
         }
 
-        self.expected_result = ["\t'key1': 'KEY1',\n", "\t'key2': 'KEY2',\n"]
+        self.expected_result = ["\t'foo\\'s bar': 'FOO\\'S BAR',\n", "\t'key1': 'KEY1',\n", "\t'key2': 'KEY2',\n"]
 
     def test_get_keys(self):
         self.assertEqual(self.expected_result, _get_exceptions(self.dictionary))
