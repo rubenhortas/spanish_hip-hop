b86e82043d083b88616b24754d48e7d659a96383
rubenhortas <rubenhortas@gmail.com>
2024-06-27 22:42:23 +0200

    Refactored get_duplicates.py and optimized

--- tools/get_duplicates.py
+++ tools/get_duplicates.py
@@ -5,8 +5,7 @@ import signal
 from tqdm import tqdm
 
 from tools.config.config import CSV_FILE, CsvPosition, CSV_DELIMITER
-from tools.crosscutting.strings import DONE, DUPLICATES, POSSIBLE_DUPLICATES, NO_DUPLICATES_FOUND, \
-    LOOKING_FOR_DUPLICATES_IN
+from tools.crosscutting.strings import DONE, DUPLICATES, NO_DUPLICATES_FOUND, LOOKING_FOR_DUPLICATES_IN, SIMILARS
 from tools.helpers.file_helpers import write_file, read_csv_file
 from tools.helpers.os_helpers import handle_sigint, clear_screen
 from tools.utils.string_utils import remove_punctuation_symbols
@@ -15,74 +14,102 @@ _OUTPUT_FILE = f"{CSV_FILE[:-4]}-{DUPLICATES.lower()}.txt"
 _MATCH_THRESHOLD = 0.9  # Seems a reasonable threshold
 
 
-def _get_duplicates(lines: list) -> (list, list):
-    lines_ = _normalize_lines(lines)
-    lines_num = len(lines_)
+class Line:
+    def __init__(self, csv_line):
+        self.id = csv_line[CsvPosition.ID.value]
+        self.artist = csv_line[CsvPosition.ARTIST.value]
+        self.title = csv_line[CsvPosition.TITLE.value]
+        self.hash = self._get_string_hash()
+        self.is_duplicate = False
+        self.duplicates = []
+        self.similar = []
+
+    def __str__(self):
+        return f"{self.id}{CSV_DELIMITER}{self.artist}{CSV_DELIMITER}{self.title}..."
+
+    def __eq__(self, other):
+        return (self.id == other.id
+                and self.artist == other.artist
+                and self.title == other.title
+                and self.hash == other.hash
+                and all(a == b for a, b in zip(self.duplicates, other.duplicates))
+                and all(a == b for a, b in zip(self.similar, other.similar)))
+
+    def has_duplicates(self):
+        return len(self.duplicates) > 0
+
+    def has_similar(self):
+        return len(self.similar) > 0
+
+    def _get_string_hash(self):
+        value = f"{self.artist}{self.title}".lower()
+        value = value.replace(' ', '')
+        value = remove_punctuation_symbols(value)
+
+        return value
+
+
+def _get_duplicates(csv_lines: list) -> list:
+    lines = []
     duplicates = []
-    possible_duplicates = []
+
+    for line in csv_lines:
+        lines.append(Line(line))
+
+    lines_num = len(lines)
     sequence_matcher = difflib.SequenceMatcher(None)
 
     for i in tqdm(range(lines_num)):
-        sequence_matcher.set_seq1(lines_[i][1])
+        current_line = lines[i]
+        sequence_matcher.set_seq1(current_line.hash)
 
         for j in range(i + 1, lines_num):
-            sequence_matcher.set_seq2(lines_[j][1])
-            match_ratio = sequence_matcher.ratio()
+            comparing_line = lines[j]
 
-            if match_ratio > _MATCH_THRESHOLD:
-                duplicate = f"{lines_[i][0].strip()} -> {lines_[j][0]}"
+            if not comparing_line.is_duplicate:
+                sequence_matcher.set_seq2(comparing_line.hash)
+                match_ratio = sequence_matcher.ratio()
 
-                if match_ratio == 1:
-                    duplicates.append(duplicate)
-                else:
-                    possible_duplicates.append(duplicate)
+                if match_ratio > _MATCH_THRESHOLD:
+                    comparing_line.is_duplicate = True
 
-                break
+                    if match_ratio == 1:
+                        current_line.duplicates.append(comparing_line)
+                    else:
+                        current_line.similar.append(comparing_line)
 
-    return duplicates, possible_duplicates
+        if current_line.has_duplicates() or current_line.has_similar():
+            duplicates.append(current_line)
 
-
-def _normalize_lines(lines: list) -> list:
-    normalized_lines = []
-
-    for line in lines:
-        line_resume_info = f"{line[CsvPosition.ID.value]}{CSV_DELIMITER}{line[CsvPosition.ARTIST.value]}{CSV_DELIMITER}{line[CsvPosition.TITLE.value]}..."
-        line_value = _normalize(line[CsvPosition.ARTIST.value], line[CsvPosition.TITLE.value])
-        line_info = (line_resume_info, line_value)
-        normalized_lines.append(line_info)
-
-    return normalized_lines
+    return duplicates
 
 
-def _normalize(artist: str, title: str) -> str:
-    value = f"{artist}{title}"
-    value = value.replace(' ', '')
-    value = remove_punctuation_symbols(value)
+def _write_output_file(lines: list) -> None:
+    if lines:
+        output_lines = []
 
-    return value
+        for line in lines:
+            output_lines.append(f"{line}:")
 
+            if line.has_duplicates():
+                output_lines.append(f"\t{DUPLICATES}:")
 
-def _write_output_file(duplicates: list, possible_duplicates: list) -> None:
-    if duplicates or possible_duplicates:
-        issues = []
+                for duplicate in line.duplicates:
+                    output_lines.append(f"\t - {duplicate}")
 
-        if duplicates:
-            issues.append(f"{DUPLICATES}:\n\n")
+                output_lines.append('')
 
-            for duplicate in duplicates:
-                issues.append(f"{duplicate}\n")
+            if line.has_similar():
+                output_lines.append(f"\t{SIMILARS}:")
 
-            issues.append('\n')
+                for similar in line.similar:
+                    output_lines.append(f"\t - {similar}")
 
-        if possible_duplicates:
-            issues.append(f"{POSSIBLE_DUPLICATES}:\n\n")
+                output_lines.append('')
 
-            for possible_duplicate in possible_duplicates:
-                issues.append(f"{possible_duplicate}\n")
+            output_lines.append('')
 
-            issues.append('\n')
-
-        write_file(_OUTPUT_FILE, issues)
+        write_file(_OUTPUT_FILE, output_lines)
         print(f"\n{DONE}")
     else:
         print(f"{NO_DUPLICATES_FOUND}")
@@ -94,6 +121,6 @@ if __name__ == '__main__':
     print(f"{LOOKING_FOR_DUPLICATES_IN} '{CSV_FILE}'...")
 
     lines = read_csv_file(CSV_FILE)[1:]
-    duplicates, possible_duplicates = _get_duplicates(lines)
+    duplicates = _get_duplicates(lines)
 
-    _write_output_file(duplicates, possible_duplicates)
+    _write_output_file(lines)
--- tools/tests/test_get_duplicates.py
+++ tools/tests/test_get_duplicates.py
@@ -1,25 +1,39 @@
 import unittest
 
-from tools.get_duplicates import _get_duplicates
+from tools.get_duplicates import _get_duplicates, Line
 
 
 class TestGetDuplicates(unittest.TestCase):
     def setUp(self):
+        line1 = ['1', 'Bob', 'Title vol.1']
+        line2 = ['2', 'Bob', 'Title vol.2']
+        line3 = ['3', 'Bob', 'Title']
+        line4 = ['4', 'Bob', 'Title@?!$']
+        line5 = ['5', 'Bob', 'Title (more words)']
+        line6 = ['6', 'Bob', 'T.i.t.l.@']
+        line7 = ['7', 'Alice', 'Another title']
+
         self.lines = [
-            ['1', 'Bob', 'Title vol.1'],
-            ['2', 'Bob', 'Title vol.2'],
-            ['3', 'Bob', 'Title'],
-            ['4', 'Bob', 'Title@?!$'],
-            ['5', 'Bob', 'Title (more words)'],
-            ['6', 'Bob', 'T.i.t.l.@'],
-            ['7', 'Alice', 'Another title']
+            line1,
+            line2,
+            line3,
+            line4,
+            line5,
+            line6,
+            line7
         ]
 
-        self.expected_duplicates = ['3,Bob,Title... -> 4,Bob,Title@?!$...']
-        self.expected_possible_duplicates = ['1,Bob,Title vol.1... -> 2,Bob,Title vol.2...',
-                                             '4,Bob,Title@?!$... -> 6,Bob,T.i.t.l.@...']
+        expected_duplicates_line1 = Line(line1)
+        expected_duplicates_line1.similar.append(Line(line2))
+
+        expected_duplicates_line2 = Line(line3)
+        expected_duplicates_line2.duplicates.append(Line(line4))
+        expected_duplicates_line2.similar.append(Line(line6))
+
+        self.expected_duplicates = [
+            expected_duplicates_line1,
+            expected_duplicates_line2
+        ]
 
     def test_get_duplicates(self):
-        duplicates, possible_duplicates = _get_duplicates(self.lines)
-        self.assertEqual(self.expected_duplicates, duplicates)
-        self.assertEqual(self.expected_possible_duplicates, possible_duplicates)
+        self.assertEqual(self.expected_duplicates, _get_duplicates(self.lines))
