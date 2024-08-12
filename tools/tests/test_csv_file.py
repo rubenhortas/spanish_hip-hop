878645532d371fcc92c88f5abb83366c0b3cafa5
rubenhortas <rubenhortas@gmail.com>
2024-08-12 12:08:27 +0200

    Adjusted tests

--- tools/tests/test_csv_file.py
+++ tools/tests/test_csv_file.py
@@ -6,8 +6,8 @@ from config.config import CsvPosition, CSV_HEADER
 class TestCsv(unittest.TestCase):
     header = CSV_HEADER
 
-    def _create_line(self, album_id: str, artist: str, title: str, preserver: str = '') -> list:
-        line = ['' for _ in range(len(self.header))]
+    def _create_line(self, album_id: str, artist: str, title: str, preserver: str = '-') -> list:
+        line = ['-' for _ in range(len(self.header))]
 
         line[CsvPosition.ID.value] = album_id
         line[CsvPosition.ARTIST.value] = artist
